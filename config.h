f2119389801260e273d87d696e080616b9cf1700
Jason Blocklove <jason.blocklove@gmail.com>
2020-03-25 11:55:58 -0400

    Removed config.def.h and changed font to IBM Plex Mono

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static int min_width = 500;                    /* minimum width when centered */
 static int fuzzy = 1;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"IBM Plex Mono:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
